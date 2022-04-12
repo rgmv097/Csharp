0a6c92aa81f59146ccb8336138a63c834bf7b39f
rgmv097 <chingiznr@code.edu.az>
2022-04-12 23:08:14 +0400

    Homeworks

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=Convert.ToInt32(Console.ReadLine());
+            if(a%2==0)
+            {
+                Console.WriteLine("Cutdur");
+            }
+            else
+            {
+                Console.WriteLine("Tekdir");
+
+            }
+        }
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console penceresinde 2 eded daxil edin. Daxil olunan 2 ededden hansinin
+            //boyuk oldugu ekrana yazilsin. Eger beraberdirse ekrana beraberdir yazilsin
+            Console.WriteLine("1 ci ededi daxilt et:");
+            int a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2 ci ededi daxilt et:");
+            int b =Convert.ToInt32(Console.ReadLine());
+
+            if (a>b)
+            {
+                Console.WriteLine("1 ci eded boyukdur - "+ a);
+            }
+            else if (b>a)
+            {
+                Console.WriteLine("2 ci eded boyukdur - " + b);
+            }
+            else
+            {
+                Console.WriteLine("ededler beraberdir");
+            }
+        }
+    }
+}
