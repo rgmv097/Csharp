ea21dc1337308b5ea64c6b965bc9d1b08d0c1b50
rgmv097 <chingiznr@code.edu.az>
2022-04-14 01:44:01 +0400

    Homework with medthod

---
+++ day-2/Task3withMethod/Task3withMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task3withMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bir string arrayinin icersinde uzunlugu 5 den boyuk olan sozleri ekrana cixardan bir alqoritm yazin.
+            //method vasitesi ile verilmish reqemden uzun olan soz ekrana cixarilsin
+            string[] arr = { "fexrin", "Chingiz", "cavid", "shahxrom", "bir","dord" };
+            
+            Console.WriteLine("Massivde daxil etdiyiniz reqemden uzun string ekrana verilsin:");
+            int divide=Convert.ToInt32(Console.ReadLine());
+            strArr(arr,divide);
+
+        }
+        public static void strArr(string[] arr,int divide)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Length>divide)
+                {
+                    Console.WriteLine(arr[i]);
+
+                }
+
+            }
+
+
+        }
+        
+    }
+}
---
+++ day-2/task4withmethod/task4withmethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task4withmethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verilmiş tam müsbət n ədədinin rəqəmlərinin cəmini tapan alqoritm (Məsələn: 214 ədədinin rəqəmləri cəmi 2+1+4 = 7)
+            int divide =Convert.ToInt32(Console.ReadLine());
+            sum(divide);
+
+        }
+        public static void sum(int divide)
+        {
+            int lastDigit;
+            int sum = default(int);
+            while (divide>0)
+            {
+                lastDigit = divide % 10;
+                divide /= 10;
+                sum+=lastDigit;
+
+
+            }
+            Console.WriteLine(sum);
+
+
+
+
+        }
+    }
+}
