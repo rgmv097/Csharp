c5e478aeababbc33b6f055b3272796f4488d5ffe
rgmv097 <chingiznr@code.edu.az>
2022-04-13 23:16:00 +0400

    Day-2 HomeWorks

---
+++ day-2/Task3/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.Bir string arrayinin icersinde uzunlugu 5 den boyuk olan sozleri ekrana cixardan bir alqoritm yazin.
+            string[] arr = { "Chingiz", "Fexrin", "Cavid", "iki", "bir" };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Length>5)
+                {
+                    Console.WriteLine(arr[i]);
+
+                }
+
+
+
+            }
+
+
+
+
+        }
+    }
+}
---
+++ day-2/Task4/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. Verilmiş tam müsbət n ədədinin rəqəmlərinin cəmini tapan alqoritm (Məsələn: 214 ədədinin rəqəmləri cəmi 2+1+4 = 7)
+            int number=Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int digit;
+            while (number>0)
+            {
+                digit = number % 10;
+                number /= 10;
+                sum+=digit;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
