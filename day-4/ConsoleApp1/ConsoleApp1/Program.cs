a1859573b09eb307424c6de366d346b92fcd13f5
rgmv097 <chingiznr@code.edu.az>
2022-04-19 00:11:17 +0400

    HomeWork

---
+++ day-4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Ededlerden ibaret array verilib, bu ededlerin en
+            //boyuyunu tapmali.  Mes: [10,8,49,17,5] Ekrana en boyuk eded yazilmalidir: 49
+            int[] arr = { 10, 8, 49, 17, 5 };
+            int max=0;
+            foreach (int i in arr)
+            {
+                if (i>max)
+                {                 
+                    max=i;
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
