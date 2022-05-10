ee171d527553f34e574bd65f77589020138ac24e
rgmv097 <chingiznr@code.edu.az>
2022-05-11 01:28:46 +0400

    HW

--- day-10/ConsoleApp1/ConsoleApp1/Program.cs
+++ day-10/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using ConsoleApp1.Interfaces;
 namespace ConsoleApp1
 {
     internal class Program
@@ -8,37 +7,39 @@ namespace ConsoleApp1
         {
         NewPlace:
             Console.WriteLine("Please choose the one");
-            Console.WriteLine("1 - Excel\n2 - Word\n3 - PDF\n4 - Clear Console\n5 - Exit");
-            int choose = Convert.ToInt32(Console.ReadLine());
-
-            switch (choose)
+            Console.WriteLine("1 - Excel\n2 - Word\n3 - PDF\n4 - Clear Console\n5 - Exit");          
+            bool res = int.TryParse(Console.ReadLine(), out int select);
+            if (res)
             {
-                case 1:
-                    MainPrintFile excel = new MainPrintFile(new Excel());
-                    Console.WriteLine("- - - - - - -");
-                    goto NewPlace;
-                case 2:
-                    MainPrintFile word = new MainPrintFile(new Word());
-                    Console.WriteLine("- - - - - - -");
-                    goto NewPlace;
-                case 3:
-                    MainPrintFile pdf = new MainPrintFile(new PDF());
-                    Console.WriteLine("- - - - - - -");
-                    goto NewPlace;
-                case 4:
-                    Console.Clear();
-                    goto NewPlace;
-                case 5:
-                    break;
-                default:
-                    Console.WriteLine("Please choose the correct variant");
-                    Console.WriteLine("- - - - - - -");
-                    break;
+                switch (select)
+                {
+                    case 1:
+                        Console.WriteLine("- - - - - - -");
+                        MainPrintFile excel = new MainPrintFile(new Excel());
+                        Console.WriteLine("- - - - - - -");
+                        goto NewPlace;
+                    case 2:
+                        Console.WriteLine("- - - - - - -");
+                        MainPrintFile word = new MainPrintFile(new Word());
+                        Console.WriteLine("- - - - - - -");
+                        goto NewPlace;
+                    case 3:
+                        Console.WriteLine("- - - - - - -");
+                        MainPrintFile pdf = new MainPrintFile(new PDF());
+                        Console.WriteLine("- - - - - - -");
+                        goto NewPlace;
+                    case 4:
+                        Console.Clear();
+                        goto NewPlace;
+                    case 5:
+                        break;
+                    default:
+                        Console.WriteLine("Please choose the correct variant");
+                        Console.WriteLine("- - - - - - -");
+                        goto NewPlace;
+                }
             }
-
-            
-
-            
+            else { Console.WriteLine("PLS select correct variant\n- - - - - - -"); goto NewPlace; }
         }
     }
 }
